d46f41f3a6a6d2a2b40eeee46a0e0cdfe6fa5ab3
Shourya Shinde <shindeshauryaanil@gmail.com>
2025-09-24 11:19:30 +0530

    Server Created

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from "express" 
+import bodyparser from "body-parser"
+
+const app = express() ;
+const port = 3000 ;
+
+app.get("/" , (req,res) => {
+  res.render("index.ejs") ;
+})
+app.listen(port , ()=> {
+  console.log(`Listening on port ${port}`) ;
+})
