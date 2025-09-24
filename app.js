658f6dfbd45e5552380428c5147e6ae21617a1f0
Shourya Shinde <shindeshauryaanil@gmail.com>
2025-09-24 13:15:59 +0530

    Added NavBar Routing

--- app.js
+++ app.js
@@ -9,7 +9,20 @@ app.use(bodyParser.urlencoded({extended : true})) ;
 
 app.get("/" , (req,res) => {
   res.render("index.ejs") ;
-})
+});
+
+app.get("/upload" , (req,res) => {
+  res.render("upload.ejs") ;
+});
+
+app.get("/transcribe" , (req,res) => {
+  res.render("transcribe.ejs") ;
+});
+
+app.get("/quiz" , (req,res) => {
+  res.render("quiz.ejs") ;
+});
+
 
 app.listen(port , ()=> {
   console.log(`Listening on port ${port}`) ;
