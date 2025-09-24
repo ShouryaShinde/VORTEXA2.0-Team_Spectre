44bd57440d7e6884140b546fbe4a394db682147d
Shourya Shinde <shindeshauryaanil@gmail.com>
2025-09-24 19:01:04 +0530

    Added Summarize.js

--- summarize.js
+++ summarize.js
@@ -6,9 +6,9 @@ const ai = new GoogleGenAI({});
 async function main() {
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
-    contents: "Explain how AI works in a few words",
+    contents: "AI is the field of computer science that focuses on creating intelligent machines that can perform tasks that usually require human intelligence." + "Summarize the text in short",
   });
-  response.render("transcribe.ejs" , {summary : response.text}) ;
+  console.log(response.text) ;
 }
 
 main();
